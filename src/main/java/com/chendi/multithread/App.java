a8a2583aed31301aaa83b644a0fb58a20da5ae61
Di Chen <chendi1025@gmail.com>
2019-06-06 17:00:28 -0400

    creat thread by extending thread class

--- src/main/java/com/chendi/multithread/App.java
+++ src/main/java/com/chendi/multithread/App.java
@@ -2,21 +2,31 @@ package com.chendi.multithread;
 
 
 
-class Runner1 implements Runnable {
+class Runner1 extends Thread {
 
     public void run() {
-        for(int i=0;i<10;i++){
+        for(int i=0;i<100;i++){
             System.out.println("Runner1: "+i);
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
 
 
-class Runner2 implements Runnable {
+class Runner2 extends Thread {
 
     public void run() {
         for(int i=0;i<10;i++){
             System.out.println("Runner2: "+i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -24,14 +34,14 @@ class Runner2 implements Runnable {
 public class App {
 
     public static void main(String[] args){
-        Thread t1= new Thread(new Runner1());
-        Thread t2= new Thread(new Runner2());
+//        Thread t1= new Thread(new Runner1());
+//        Thread t2= new Thread(new Runner2());
+
+        Runner1 t1 = new Runner1();
+        Runner2 t2 = new Runner2();
 
         t1.start();
         t2.start();
-
-
-
 
     }
 }
