d15cb241b86120bc64745e09eeb5256186a01197
Di Chen <chendi1025@gmail.com>
2019-07-08 12:22:39 -0400

    Semaphore exercise

---
+++ src/main/java/com/chendi/multithread/SemaphoreEx.java
@@ -0,0 +1,18 @@
+package com.chendi.multithread;
+
+
+
+
+
+public class Semaphore {
+    //maintains a set of permits;
+    //acquire() if a permit is available, then takes it
+    //release() adds a permit
+    //Semaphore( int permits, boolean fair)
+
+    public static void main(String[] args){
+        
+    }
+
+}
+
