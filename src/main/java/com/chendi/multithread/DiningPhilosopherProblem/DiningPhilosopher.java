23a50bad42874d2e3256925d80c265d7b5e9bb68
Di Chen <chendi1025@gmail.com>
2019-07-10 16:55:40 -0400

    philosopher-chopstics problem solving; implemented Philosopher class;tested

--- src/main/java/com/chendi/multithread/DiningPhilosopherProblem/DiningPhilosopher.java
+++ src/main/java/com/chendi/multithread/DiningPhilosopherProblem/DiningPhilosopher.java
@@ -1,8 +1,50 @@
 package com.chendi.multithread.DiningPhilosopherProblem;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * assign locks on each chopstick
  */
 
 public class DiningPhilosopher {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ExecutorService executorService = null;
+        Philosopher[] philosophers = null;
+
+        try{
+            philosophers = new Philosopher[Constants.NUMBER_OF_PHILOSOPHERS];//create an array of philosophers
+            Chopstick chopsticks[] = new Chopstick[Constants.NUMBER_OF_CHOPSTICKS];
+
+            for(int i=0;i<Constants.NUMBER_OF_CHOPSTICKS;i++)
+                chopsticks[i] = new Chopstick(i); //initialize the chopsticks array
+
+            executorService= Executors.newFixedThreadPool(Constants.NUMBER_OF_PHILOSOPHERS);// assign threads to every single philosopher
+
+            for(int i=0;i<Constants.NUMBER_OF_PHILOSOPHERS;i++){
+                philosophers[i]= new Philosopher(i,chopsticks[i],chopsticks[(i+1) % Constants.NUMBER_OF_CHOPSTICKS]);
+                executorService.execute(philosophers[i]);
+            }
+
+            Thread.sleep(Constants.SIMULATION_RUNNING_TIME);
+
+            for(Philosopher p:philosophers){
+                p.setFull(true);
+            }
+        } finally {
+            executorService.shutdown();
+
+            while(!executorService.isTerminated()){
+                Thread.sleep(1000);
+            }
+
+            for(Philosopher p:philosophers){
+                System.out.println(p+" eats "+ p.getEatingCounter());
+            }
+
+        }
+
+    }
 }
--- src/main/java/com/chendi/multithread/DiningPhilosopherProblem/Philosopher.java
+++ src/main/java/com/chendi/multithread/DiningPhilosopherProblem/Philosopher.java
@@ -1,4 +1,65 @@
 package com.chendi.multithread.DiningPhilosopherProblem;
 
-public class Philosopher {
+import java.util.Random;
+
+public class Philosopher implements Runnable {
+
+    private int id;
+    private Chopstick leftChopstick;
+    private Chopstick rightChopstick;
+    private Random random;
+    private int eatingCounter;
+    private volatile boolean isFull = false;//get the var from the main memory not the cache
+
+    public Philosopher(int id, Chopstick leftChopstick, Chopstick rightChopstick){
+        this.id=id;
+        this.leftChopstick=leftChopstick;
+        this.rightChopstick= rightChopstick;
+        this.random = new Random();
+    }
+
+
+    public void run() {
+
+        try {
+            while(!isFull){
+                think();
+                if(leftChopstick.pickUp(State.LEFT,this)){
+                    if(rightChopstick.pickUp(State.RIGHT,this)){
+                        eat();
+                        rightChopstick.putDown(this,State.RIGHT);
+                    }
+                    leftChopstick.putDown(this,State.LEFT);
+                }
+            }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+        }
+    }
+
+
+    public void setFull(boolean isFull) {
+        this.isFull = isFull;
+    }
+
+    @Override
+    public String toString() {
+        return "Philosopher " +
+                "id=" + id ;
+    }
+
+    private void think() throws InterruptedException{
+        System.out.println("Philosopher is thinking...");
+        Thread.sleep(random.nextInt(1000));
+    }
+    private void eat() throws InterruptedException{
+        System.out.println("Philosopher is eating...");
+        this.eatingCounter++;
+        Thread.sleep(random.nextInt(1000));
+    }
+    public int getEatingCounter(){
+        return eatingCounter;
+    }
+
+
 }
